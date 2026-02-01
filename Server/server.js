0b5c6ae41d58a7e90a710023529c493128e6cf6e
Janani <janujanu2403@gmail.com>
2026-02-01 23:37:07 +0530

    Serve client folder for frontend on Railway

--- Server/server.js
+++ Server/server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static("client"));
+app.use(express.static("../client"));
 
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
@@ -34,9 +34,17 @@ io.on("connection", (socket) => {
   });
 });
 
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-const PORT = process.env.PORT || 3000; 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+
+
+
+
+
+
+
+
+
 
